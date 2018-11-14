f77c0a1a39bc46534fa74b4e72e709a299009bf2
Xander Ge <xander.q.ge@gmail.com>
2018-11-14 12:19:28 -0600

    Began Search Activity

---
+++ app/src/main/java/lin_ge/restaurantadvisor/SearchActivity.java
@@ -0,0 +1,33 @@
+package lin_ge.restaurantadvisor;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SearchActivity extends ListActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            search(query);
+        }
+    }
+
+    private static void search(String term) {
+
+    }
+
+
+}
