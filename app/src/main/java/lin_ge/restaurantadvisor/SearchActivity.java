0ba8a7a0a35be6623bacc3bd4faeb7710ffdd9bd
Xander Ge <xander.q.ge@gmail.com>
2018-11-18 22:55:31 -0600

    WIP Search Activity

--- app/src/main/java/lin_ge/restaurantadvisor/SearchActivity.java
+++ app/src/main/java/lin_ge/restaurantadvisor/SearchActivity.java
@@ -1,33 +1,28 @@
 package lin_ge.restaurantadvisor;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.app.SearchManager;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SearchView;
 
-public class SearchActivity extends ListActivity{
+public class SearchActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
 
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            search(query);
-        }
+        return true;
     }
-
-    private static void search(String term) {
-
-    }
-
-
 }
