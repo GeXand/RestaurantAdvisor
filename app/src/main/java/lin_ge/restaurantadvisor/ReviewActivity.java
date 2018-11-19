2c5b89c1270942e85d10671ef4799f8802907abd
Xander Ge <xander.q.ge@gmail.com>
2018-11-18 22:56:46 -0600

    Fixed Field initialization

--- app/src/main/java/lin_ge/restaurantadvisor/ReviewActivity.java
+++ app/src/main/java/lin_ge/restaurantadvisor/ReviewActivity.java
@@ -1,5 +1,6 @@
 package lin_ge.restaurantadvisor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,20 +10,28 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class ReviewActivity extends AppCompatActivity {
-    private RatingBar mRBar = (RatingBar) findViewById(R.id.ratingBar);
-    private Button mButton = (Button) findViewById(R.id.Submit);
-    private TextView mTView = (TextView) findViewById(R.id.textView);
-    private EditText mEText = (EditText) findViewById(R.id.editText);
+    private RatingBar mRBar;
+    private Button mButton;
+    private TextView mTView;
+    private EditText mEText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+
+        mRBar = (RatingBar) findViewById(R.id.ratingBar);
+        mButton = (Button) findViewById(R.id.Submit);
+        mTView = (TextView) findViewById(R.id.textView);
+        mEText = (EditText) findViewById(R.id.editText);
+
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Take in Review list from Restaurant and add review to it
                 finish();
+                startActivity(new Intent(ReviewActivity.this, RestaurantActivity.class));
             }
         });
     }
