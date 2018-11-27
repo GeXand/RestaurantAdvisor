2891b8a1af60095c54c89fb2b8841e6f91239820
Michael <michael.lin2032@gmail.com>
2018-11-27 09:18:22 -0600

    Added PriorityQueue

---
+++ app/src/main/java/lin_ge/restaurantadvisor/PriorityQueue.java
@@ -0,0 +1,56 @@
+package lin_ge.restaurantadvisor;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class PriorityQueue {
+
+    private ArrayList<Restaurant> list;
+
+    public PriorityQueue()
+    {
+        list = new ArrayList<Restaurant>();
+    }
+
+    public void enqueue(Restaurant restaurant)
+    {
+        list.add(restaurant);
+    }
+
+    public boolean isFull()
+    {
+        return false;
+    }
+
+    public boolean isEmpty()
+    {
+        return list.size() == 0;
+    }
+
+    public Restaurant dequeue(Comparator<String> c, String search)
+    {
+        if(isEmpty())
+            return null;
+        for(int i = 0; i < list.size(); i++)
+        {
+            Restaurant temp = list.get(i);
+            if(c.compare(temp.toString(), search) > 0)
+            {
+                list.remove(temp);
+                return temp;
+            }
+        }
+        return null;
+    }
+
+    public boolean contains(String search)
+    {
+        for(Restaurant r: list)
+        {
+            if(r.toString().contains(search))
+                return true;
+        }
+        return false;
+    }
+}
+
