57a6285471aa55be85b110c94c4a42b4cd43458c
Xander Ge <xander.q.ge@gmail.com>
2018-12-05 18:05:15 -0600

    Started Json Parsing

---
+++ app/src/main/java/lin_ge/restaurantadvisor/databases/DatabaseLoader.java
@@ -0,0 +1,31 @@
+package lin_ge.restaurantadvisor.databases;
+
+import android.util.JsonReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
+import lin_ge.restaurantadvisor.components.Restaurant;
+
+import static java.lang.System.in;
+
+public class DatabaseLoader {
+    public static void fillDatabase(String restaurantJsonFile) throws Exception {
+        Object JSONFile = new JSONParser().parse(new FileReader(restaurantJsonFile));
+        JSONObject jsonObject = (JSONObject) JSONFile;
+        JSONArray restaurants = (JSONArray) jsonObject.get("restaurants");
+
+        for (int i = 0; i < restaurants.size(); i++) {
+            JSONObject joCur = (JSONObject) restaurants.get(i);
+            JSONObject curRestaurant = (JSONObject) joCur.get("restaurant");
+
+
+
+        }
+    }
+}
